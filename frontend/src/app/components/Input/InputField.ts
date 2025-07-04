9c06144ae3a77296aae29774fb8a6921a23a3658
Alexander Heinisch <alexander.heinisch@rise-world.com>
2025-07-05 01:21:43 +0200

    MM-#6: add visibilty eye to password fields

--- frontend/src/app/components/Input/InputField.ts
+++ frontend/src/app/components/Input/InputField.ts
@@ -18,146 +18,178 @@ export enum InputType {
     imports: [CommonModule, FormsModule],
     selector: 'input-field',
     template: `
-    <div [class]="marginBottom">
-      <label *ngIf="label && type !== InputType.checkbox" [attr.for]="id" class="block mb-2 text-base text-primary">{{
-        label
-      }}</label>
+      <div [class]="marginBottom">
+        <label *ngIf="label && type !== InputType.checkbox" [attr.for]="id" class="block mb-2 text-base text-primary">{{
+            label
+          }}</label>
 
-      <ng-container [ngSwitch]="type">
-        <input
-          *ngSwitchCase="InputType.text"
-          [type]="type"
-          [id]="id"
-          [name]="name"
-          [attr.aria-label]="ariaLabel"
-          [attr.placeholder]="placeholder"
-          [required]="required"
-          [disabled]="disabled"
-          [ngClass]="inputClassesWFullDefault"
-          [(ngModel)]="value"
-          (ngModelChange)="onValueChange($event)"
-          autocomplete="off"
-        />
+        <ng-container>
+          @switch (type) {
+            @case (InputType.text) {
+              <input
+                  [type]="type"
+                  [id]="id"
+                  [name]="name"
+                  [attr.aria-label]="ariaLabel"
+                  [attr.placeholder]="placeholder"
+                  [required]="required"
+                  [disabled]="disabled"
+                  [ngClass]="inputClassesWFullDefault"
+                  [(ngModel)]="value"
+                  (ngModelChange)="onValueChange($event)"
+                  autocomplete="off"
+              />
+            }
+            @case (InputType.password) {
+              <div class="flex items-center">
+                <input
+                    [type]="passwordHidden ? type : 'text'"
+                    [id]="id"
+                    [name]="name"
+                    [attr.aria-label]="ariaLabel"
+                    [attr.placeholder]="placeholder"
+                    [required]="required"
+                    [ngClass]="inputClassesWFullDefault"
+                    class="rounded-r-none"
+                    [disabled]="disabled"
+                    [(ngModel)]="value"
+                    [attr.minlength]="minlength"
+                    (ngModelChange)="onValueChange($event)"
+                />
+                <span
+                    class="border border-gray-300 rounded-l-none bg-gray-200 p-2 text-[1rem] focus:border-primary rounded-md"
+                    (click)="passwordHidden = !passwordHidden"
+                >
+                  <i
+                      class="fa"
+                      [ngClass]="{
+                        'fa-eye-slash': !passwordHidden,
+                        'fa-eye': passwordHidden
+                      }"
+                  ></i>
+                </span>
+              </div>
+            }
+            @case (InputType.number) {
+              <input
+                  [type]="type"
+                  [id]="id"
+                  [name]="name"
+                  [attr.aria-label]="ariaLabel"
+                  [attr.placeholder]="placeholder"
+                  [required]="required"
+                  [disabled]="disabled"
+                  [ngClass]="inputClassesWFullDefault"
+                  [(ngModel)]="value"
+                  (ngModelChange)="onValueChange($event)"
+                  [attr.min]="1"
+              />
 
-        <input
-          *ngSwitchCase="InputType.password"
-          [type]="type"
-          [id]="id"
-          [name]="name"
-          [attr.aria-label]="ariaLabel"
-          [attr.placeholder]="placeholder"
-          [required]="required"
-          [disabled]="disabled"
-          [ngClass]="inputClassesWFullDefault"
-          [(ngModel)]="value"
-          [attr.minlength]="minlength"
-          (ngModelChange)="onValueChange($event)"
-        />
+            }
+            @case (InputType.email) {
+              <input
+                  [type]="type"
+                  [id]="id"
+                  [name]="name"
+                  [attr.aria-label]="ariaLabel"
+                  [attr.placeholder]="placeholder"
+                  [required]="required"
+                  [disabled]="disabled"
+                  [ngClass]="inputClassesWFullDefault"
+                  [(ngModel)]="value"
+                  (ngModelChange)="onEmailChange($event)"
+                  [pattern]="emailPattern"
+              />
 
-        <input
-          *ngSwitchCase="InputType.number"
-          [type]="type"
-          [id]="id"
-          [name]="name"
-          [attr.aria-label]="ariaLabel"
-          [attr.placeholder]="placeholder"
-          [required]="required"
-          [disabled]="disabled"
-          [ngClass]="inputClassesWFullDefault"
-          [(ngModel)]="value"
-          (ngModelChange)="onValueChange($event)"
-          [attr.min]="1"
-        />
+            }
+            @case (InputType.select) {
+              <select
+                  [id]="id"
+                  [name]="name"
+                  [attr.aria-label]="ariaLabel"
+                  [required]="required"
+                  [disabled]="disabled"
+                  [ngClass]="inputClassesWFullDefault"
+                  [(ngModel)]="value"
+                  (ngModelChange)="onValueChange($event)"
+              >
+                <option *ngFor="let option of options" [value]="toSelectKey(option)">{{ toSelectLabel(option) }}
+                </option>
+              </select>
 
-        <input
-          *ngSwitchCase="InputType.email"
-          [type]="type"
-          [id]="id"
-          [name]="name"
-          [attr.aria-label]="ariaLabel"
-          [attr.placeholder]="placeholder"
-          [required]="required"
-          [disabled]="disabled"
-          [ngClass]="inputClassesWFullDefault"
-          [(ngModel)]="value"
-          (ngModelChange)="onEmailChange($event)"
-          [pattern]="emailPattern"
-        />
+            }
+            @case (InputType.date) {
+              <input
+                  [type]="type"
+                  [id]="id"
+                  [name]="name"
+                  [attr.aria-label]="ariaLabel"
+                  [attr.placeholder]="placeholder"
+                  [disabled]="disabled"
+                  [ngClass]="inputClassesWFullDefault"
+                  [(ngModel)]="value"
+                  (ngModelChange)="onValueChange($event)"
+              />
 
-        <select
-          *ngSwitchCase="InputType.select"
-          [id]="id"
-          [name]="name"
-          [attr.aria-label]="ariaLabel"
-          [required]="required"
-          [disabled]="disabled"
-          [ngClass]="inputClassesWFullDefault"
-          [(ngModel)]="value"
-          (ngModelChange)="onValueChange($event)"
-        >
-          <option *ngFor="let option of options" [value]="toSelectKey(option)">{{ toSelectLabel(option) }}</option>
-        </select>
+            }
+            @case (InputType.textarea) {
+              <textarea
+                  [id]="id"
+                  [name]="name"
+                  [attr.aria-label]="ariaLabel"
+                  [attr.placeholder]="placeholder"
+                  [required]="required"
+                  [disabled]="disabled"
+                  [ngClass]="inputClassesWFullDefault"
+                  [rows]="rows"
+                  [(ngModel)]="value"
+                  (ngModelChange)="onValueChange($event)"
+              ></textarea>
 
-        <input
-          *ngSwitchCase="InputType.date"
-          [type]="type"
-          [id]="id"
-          [name]="name"
-          [attr.aria-label]="ariaLabel"
-          [attr.placeholder]="placeholder"
-          [disabled]="disabled"
-          [ngClass]="inputClassesWFullDefault"
-          [(ngModel)]="value"
-          (ngModelChange)="onValueChange($event)"
-        />
-
-        <textarea
-          *ngSwitchCase="InputType.textarea"
-          [id]="id"
-          [name]="name"
-          [attr.aria-label]="ariaLabel"
-          [attr.placeholder]="placeholder"
-          [required]="required"
-          [disabled]="disabled"
-          [ngClass]="inputClassesWFullDefault"
-          [rows]="rows"
-          [(ngModel)]="value"
-          (ngModelChange)="onValueChange($event)"
-        ></textarea>
-
-        <div *ngSwitchCase="InputType.checkbox" class="flex items-center space-x-3">
-          <input
-            [type]="type"
-            [id]="id"
-            [name]="name"
-            [attr.aria-label]="ariaLabel"
-            [attr.placeholder]="placeholder"
-            [disabled]="disabled"
-            [ngClass]="inputClasses"
-            [ngModel]="value"
-            [checked]="value"
-            (change)="onCheckboxChange($event)"
-            class="w-4 h-4"
-          />
-          <label *ngIf="label" [attr.for]="id" class="text-sm text-neutral-700" [ngClass]="value ? labelStyling : ''">
-            {{ label }}
-          </label>
-        </div>
-        <div *ngIf="required" class="mt-1 h-3">
-          <div
-            *ngIf="
-              form && form.controls[name]?.invalid && (form.controls[name]?.dirty || !form.controls[name]?.untouched)
-            "
-          >
-            <p *ngIf="form.controls[name]?.errors?.['required']" class="text-xs text-red-600">*Field is mandatory.</p>
-          </div>
-          <div *ngIf="value && isEmailInvalid">
-            <p class="text-xs text-red-600">Invalid email format.</p>
-          </div>
-        </div>
-      </ng-container>
-    </div>
-  `,
+            }
+            @case (InputType.checkbox) {
+              <div class="flex items-center space-x-3">
+                <input
+                    [type]="type"
+                    [id]="id"
+                    [name]="name"
+                    [attr.aria-label]="ariaLabel"
+                    [attr.placeholder]="placeholder"
+                    [disabled]="disabled"
+                    [ngClass]="inputClasses"
+                    [ngModel]="value"
+                    [checked]="value"
+                    (change)="onCheckboxChange($event)"
+                    class="w-4 h-4"
+                />
+                @if (label) {
+                  <label [attr.for]="id" class="text-sm text-neutral-700"
+                         [ngClass]="value ? labelStyling : ''">
+                    {{ label }}
+                  </label>
+                }
+              </div>
+              @if (required) {
+                <div class="mt-1 h-3">
+                  @if (form && form.controls[name]?.invalid && (form.controls[name]?.dirty || !form.controls[name]?.untouched)) {
+                    <div>
+                      @if (form.controls[name]?.errors?.['required']) {
+                        <p class="text-xs text-red-600">*Field is mandatory.</p>
+                      }
+                    </div>
+                  }
+                  @if (value && isEmailInvalid) {
+                    <div>
+                      <p class="text-xs text-red-600">Invalid email format.</p>
+                    </div>
+                  }
+                </div>
+              }
+            }
+          }
+        </ng-container>
+      </div>
+    `,
     styles: [],
     viewProviders: [{ provide: ControlContainer, useExisting: NgForm }]
 })
@@ -186,6 +218,7 @@ export class InputFieldComponent {
   InputType = InputType;
   isEmailInvalid: boolean = false;
   emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // email validation pattern
+  passwordHidden = true;
 
   constructor(private stringFormattingService: StringFormattingService) {}
 
